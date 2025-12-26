5ba0cd04269cc871459a2e4ccacde627750b9a70
Peter Kidwell <peterkidwell@gmail.com>
2025-12-26 23:02:08 +0000

    Add Writing section Fixes #14

---
+++ app/Writing/page.js
@@ -0,0 +1,110 @@
+'use client'
+
+import { Box, Typography, Link, Popover, Paper } from '@mui/material';
+import { List, ListItem, ListItemButton, ListItemText, ListItemIcon } from '@mui/material';
+import ArticleIcon from '@mui/icons-material/Article';
+
+import PaddedP from '_PaddedP';
+import React, { useEffect } from 'react';
+
+function Writing() {
+
+    const _stories = [
+        {'title': 'Lie Detector Test', 'filename': 'lie-detector.json'},
+        {'title': 'Why Me?', 'filename': 'why-me.json'}
+    ];
+
+    const [openStory, setOpenStory] = React.useState(null);
+    const [storyData, setStoryData] = React.useState([]);
+
+    useEffect( () => {
+        var allTheStories = [];
+        _stories.map( (st) => {
+            fetch('_stories/' + st.filename)
+            .then((response) => {
+                if(!response.ok) {
+                    throw new Error(`http error! ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                allTheStories.push(data);
+            })
+        } );
+        setStoryData(allTheStories);
+    }, []);
+
+    const handleClick = (event) => {
+        setOpenStory(event.currentTarget.id);
+        console.log(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setOpenStory(null);
+    }
+
+    return (
+        <Box sx={{'my': '2rem'}}>
+            <Typography sx={{'marginY':'1rem'}} component='h1' variant='h1'>Microfiction</Typography>
+            <PaddedP variant='body1'>
+                I love to write. In my <a href="/Resume">day job</a>, I am the odd person who actually 
+                enjoys writing documentation. But in my free time, I have taken up writing <em>microfiction</em>.
+                Microfiction is, essentially, writing an <strong>extremely</strong> short story, usually 
+                under 300 words in total. I love the craft of it: there is such a limited space in which to 
+                tell a full story arc, so every single word counts. 
+            </PaddedP>
+            <PaddedP variant='body1'>
+                A very long time ago, when the World Wide Web was nothing more than a cobweb, I
+                hosted a site where I published my own fiction, and even got a few brave souls
+                to allow me to post theirs as well. These days, I have been submitting my work
+                to a few sites and contests, to see where my ideas might land. I have competed
+                in the <Link href="https://www.nycmidnight.com/250" target="_blank">NYC Midnight 
+                Microfiction (250 words)</Link> contest, where I got as far as 1st place in Round 
+                1 back in 2021, and in  
+                the <Link href="https://www.bathflashfictionaward.com/" target="_blank">Bath Flash 
+                Fiction Awards</Link> (where I was, ahem, not quite as successful). I have also
+                submitted work  
+                to <Link href="https://101words.org/" target="_blank">101words.org</Link> &mdash; 
+                fingers crossed!
+            </PaddedP>
+            <PaddedP variant='body1'>
+                If I&rsquo;ve made you curious, you can take a look at a couple of my pieces here.
+                (I&rsquo;ll add more over time!)
+            </PaddedP>
+            <List>
+                {_stories.map( (st, st_ix) => (
+                    <ListItem disablePadding key={'story-' + st_ix} >
+                        <ListItemButton onClick={handleClick} id={st_ix}>
+                            <ListItemIcon><ArticleIcon/></ListItemIcon>
+                            <ListItemText>{st.title}</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                )
+                )}    
+            </List>
+            {
+                storyData.map( (st, st_ix) => (
+                    <Popover key={'story-pop-' + st_ix} id={'story-pop-' + st_ix} 
+                            slotProps={{paper: {elevation: 5}}}
+                            sx={{'& .MuiPopover-paper': { maxWidth: '80%', maxHeight: '80%' }}}
+                            anchorReference="anchorPosition"
+                            anchorPosition={{top: '100', left: '100'}} 
+                            anchorOrigin={{vertical: 'top', horizontal: 'left' }}
+                            open={openStory==st_ix}  onClose={handleClose}>
+                        <Paper sx={{bgcolor: 'wheat', padding: '10px'}}>
+                            <Typography component='h1' variant='h1'>{st.title}</Typography>
+                            <Typography component='p' variant='body2' sx={{marginBottom: '10px'}}>
+                                <em>{st.comment}</em>
+                            </Typography>
+                            {st.paras.map( (p, p_ix) => (
+                                <PaddedP key={'story-'+st_ix+'-p-'+p_ix} variant='body1'>{p}</PaddedP>
+                            ))}
+                        </Paper>
+                    </Popover>
+                ))
+            }
+        </Box>
+    );
+}
+
+export default Writing;
